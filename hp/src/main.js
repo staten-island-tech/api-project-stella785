e9274f7eaf73ab9fa2a278818d9f551066a718b4
stella785 <stellac35@nycstudents.net>
2025-12-11 14:00:46 -0500

    ..

---
+++ hp/src/main.js
@@ -0,0 +1,19 @@
+import './style.css'
+
+async function getData(poke) {
+    try {
+        //get data from api
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`);
+        if (response.status != 200) {
+            throw new Error(response);
+        } else {
+            //converts response into json we can use
+            const data = await response.json();
+            //console.log(data);
+            document.getElementById("api-response").textContent = data.name;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+getData("Squirtle");
