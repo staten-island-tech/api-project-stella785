821b57feeff2f1ee499bb2a5dc83723b3ee59a5b
stella785 <stellac35@nycstudents.net>
2025-12-12 14:01:37 -0500

    ...

--- hp/src/main.js
+++ hp/src/main.js
@@ -1,5 +1,6 @@
 import './style.css'
 
+/////example
 async function getData(poke) {
     try {
         //get data from api
@@ -17,3 +18,36 @@ async function getData(poke) {
     }
 }
 getData("Squirtle");
+
+/////my code
+
+let data = [
+    {
+        id: "",
+        name: "",
+        incantation: "",
+        effect: "",
+        canBeVerbal: "",
+        type: "",
+        ligt: "",
+        creator: "" 
+    }
+]
+
+async function getData(spells) {
+    try {
+        //get data from api
+        const response = await fetch(`https://wizard-world-api.herokuapp.com/Spells${spells}`);
+        if (response.status != 200) {
+            throw new Error(response);
+        } else {
+            //converts response into json we can use
+            const data = await response.json();
+            //console.log(data);
+            document.getElementById("api-response").textContent = data.name;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+getData("Opening Charm");
